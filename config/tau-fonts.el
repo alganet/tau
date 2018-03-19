b2e46f5fea13c3e450a427bc73d06d0e3a955c76
Alexandre Gaigalas <alexandre.gaigalas@easytaxi.com.br>
2018-03-19 16:36:49 -0300

    Cleanup, adding OS X keymap, removed mouse settings

--- config/tau-fonts.el
+++ config/tau-fonts.el
@@ -4,21 +4,4 @@
 (prefer-coding-system 'utf-8)
 
 
-;; Use variable width font faces in current buffer
-(defun my-buffer-face-mode-variable ()
-  "Set font to a variable width (proportional) fonts in current buffer"
-  (interactive)
-  (setq buffer-face-mode-face '(:family "Ubuntu Condensed" :height 115))
-  (buffer-face-mode))
-
-;; Set default font faces for Info and ERC modes
-(add-hook 'neotree-mode-hook 'my-buffer-face-mode-variable)
-
-(set-default-font "Monoid HalfTight-10")
-(set-face-attribute 'default t :font "Monoid HalfTight-10" )
-(set-face-attribute 'default nil :font "Monoid HalfTight-10" )
-(set-face-attribute 'variable-pitch t :font "Ubuntu Condensed-11" )
-(set-face-attribute 'variable-pitch nil :font "Ubuntu Condensed-11" )
-
-
 (provide 'tau-fonts)
--- config/tau-mouse.el
+++ config/tau-mouse.el
@@ -6,22 +6,4 @@
 (if window-system
     (scroll-bar-mode -1))
 
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll one line at a time
-(setq mouse-drag-copy-region nil)
-
-(use-package smooth-scroll
-  :ensure t
-  :config
-  (smooth-scroll-mode 1)
-  (setq smooth-scroll/vscroll-step-size 10)
-  )
-
-
-(setq scroll-margin 3
-      scroll-conservatively 0
-      scroll-up-aggressively 0.01
-      scroll-down-aggressively 0.01)
-
 (provide 'tau-mouse)
